34f60b55eb0b3f8d2ec956ff44b18ba1c1032898
FelixvL <digitaleservice@gmail.com>
2022-03-04 13:28:29 +0100

    inloggen gelijk

--- felix.js
+++ felix.js
@@ -1,4 +1,12 @@
 function checkInloggen(){
   console.log("ik ga checkinloggen implementeren");
+  let userid = localStorage.getItem("wastelessuserid");
+  let inlogknop = document.getElementById("inlogbutton");
+  console.log(inlogknop);
+  if(userid == undefined){
+    inlogknop.innerHTML = "Inloggen"
+  }else{
+    inlogknop.innerHTML = "Voorraadkast"
+  }
 }
-window.onload = checkInloggen();
+window.onload = checkInloggen;
