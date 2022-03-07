9242ffdee2af42efe3c70c20a369948bb65cd95b
Deusie <43952614+Deusie@users.noreply.github.com>
2022-03-07 11:15:34 +0100

    aanbieden en cancelen via de voorraadkast

--- voorraadkast.js
+++ voorraadkast.js
@@ -1,7 +1,18 @@
 
 
-function offerItem(userId) {
-    //hier komt een functie om een item uit de voorraadkast aan te bieden 
+function updateOffered(itemId, offered) {
+    fetch(`${url}/updateoffered/${itemId}/${offered}`, {//// =====>> nieuw endpoint invullen
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    .then(() => {
+        getAllItemsSortedByDate();
+    })
+    .catch(error => {
+        alert('Er is iets fouts gegaan');
+    });
 }
 
 function createItem() {
@@ -46,8 +57,14 @@ function getAllItemsSortedByDate() {
         <img src="kaas.jpg" class="card-img-top" alt="...">
             <div class="card-body">
             <h5 class="card-title">${data[y].name}  
-            <button type="button" type="button" class="btn btn-success" onclick="offerItem(${data[y].id})">aanbieden</button>
-            <button type="button" type="button" class="btn btn-danger" onclick="deleteItem(${data[y].id})">
+            `
+            if(!data[y].offered){
+                resultString += `<button type="button" type="button" class="btn btn-success me-1" onclick="updateOffered(${data[y].id}, true)">aanbieden</button>`
+            }else{
+                resultString += `<button type="button" type="button" class="btn btn-primary me-1" onmouseout="onHoverCancel(this, false)" onmouseover="onHoverCancel(this, true)" onclick="updateOffered(${data[y].id}, false)">Aangeboden</button>`
+            }
+            
+            resultString += `<button type="button" type="button" class="btn btn-danger" onclick="deleteItem(${data[y].id})">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                 <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
@@ -78,6 +95,19 @@ function deleteItem(userId) {
     .catch(error => {
         alert('Er is iets fouts gegaan');
     });
+}
+
+function onHoverCancel(x, bool){
+    if(bool){
+        x.classList.remove("btn-primary")
+        x.classList.add("btn-danger")
+        x.innerHTML = "Cancel";
+    }else{
+        x.classList.remove("btn-danger")
+        x.classList.add("btn-primary")
+        x.innerHTML = "Aangeboden";
+    }
+    
 }
 
 getAllItemsSortedByDate();
