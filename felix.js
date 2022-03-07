d119b40b5c969be2236a2e9923140bb7d8f88bdc
Tom <tvanlente@gmail.com>
2022-03-07 10:17:17 +0100

    login werkt

--- felix.js
+++ felix.js
@@ -1,12 +1,22 @@
 function checkInloggen(){
   console.log("ik ga checkinloggen implementeren");
-  let userid = localStorage.getItem("wastelessuserid");
+  let userid = localStorage.getItem("userid");
   let inlogknop = document.getElementById("inlogbutton");
   console.log(inlogknop);
-  if(userid == undefined){
-    inlogknop.innerHTML = "Inloggen"
+  if(userid == null){
+    inlogknop.innerHTML = "Inloggen";
+    inlogknop.onclick = naarInloggen;
   }else{
-    inlogknop.innerHTML = "Voorraadkast"
+    inlogknop.innerHTML = "Voorraadkast";
+    inlogknop.onclick = naarVoorraadKast;
   }
 }
 window.onload = checkInloggen;
+
+function naarInloggen(){
+  window.location = "login.html";
+  // localStorage.setItem("wastelessuserid, 3");
+}
+function naarVoorraadKast(){
+  window.location = "voorraadkast.html";
+}
