89d6f69443a06997296fab7300dfcf84fe4435f5
Marianne de Jong <mariannedejong@hotmail.com>
2022-03-08 09:32:40 +0100

    index

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
-function getSubmittedItems(){
-fetch(url+"/getoffereditemlist")
-.then((response)=>{
-  return response.json();
-})
-.then((data)=>{
-  let resultString="";
-  for(let y= 0; y<data.length; y++){
-    resultString += `
+function claimItem(itemId) {
+  let result = fetch(url + '/claim/' + itemId + '/createclaim', {
+    method: 'POST'
+
+  })
+}
+
+let x = 5
+
+function localVariable(){
+  
+}
+
+
+function getSubmittedItems() {
+  fetch(url + "/getoffereditemlist")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let resultString = "";
+      for (let y = 0; y < data.length; y++) {
+        resultString += `
     <div class="col">
       <div class="card">
         <img src="kaas.jpg" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">${data[y].name}  
-            <button type="button" type="button" class="btn btn-success" onclick="aanbieden(${data[y].id})">Reageer</button>
-            </h5>
+            <button type="button" type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="localVariable()">Claim!</button>
             <p class="card-text">
               houdbaarheidsdatum: ${data[y].expirationDate}
             </p>
           </div>
         </div>
       </div>`;
-  }
-  document.getElementById("itemList").innerHTML = resultString;
-})
+      }
+      document.getElementById("itemList").innerHTML = resultString;
+    })
 }
+
+
+
+// onclick="claimItem(${data[y].id})
